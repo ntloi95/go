4722fdecb26c7093d527d7ee4073591db3798fc8
ntloi <ntloi@fossil.com>
2020-06-22 23:00:00 +0700

    1370B

--- 1370/B/main.go
+++ 1370/B/main.go
@@ -8,15 +8,11 @@ import (
 
 var nTest, n int
 
-func gcd(x, y int) int {
-	for y != 0 {
-		t := y
-		y = x % y
-		x = t
-	}
-
-	return x
+type pair struct {
+	val int
+	id  int
 }
+
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -24,8 +20,32 @@ func main() {
 	fmt.Fscan(in, &nTest)
 	for t := 0; t < nTest; t++ {
 		fmt.Fscan(in, &n)
+		odd := []int{}
+		even := []int{}
+		var x int
+		for i := 0; i < n*2; i++ {
+			fmt.Fscan(in, &x)
 
-		fmt.Fprintln(out, gcd(n/2, n/2*2))
+			if x&1 == 0 {
+				even = append(even, i+1)
+			} else {
+				odd = append(odd, i+1)
+			}
+		}
+
+		nOdd := len(odd) / 2 * 2
+		nEven := 2*n - 2 - nOdd
+		if nEven < 0 {
+			nEven = 0
+			nOdd -= 2
+		}
+		// fmt.Println("AHIHI", nOdd, nEven)
+		for i := 0; i < nOdd/2; i++ {
+			fmt.Fprintln(out, odd[i*2], odd[i*2+1])
+		}
+		for i := 0; i < nEven/2; i++ {
+			fmt.Fprintln(out, even[i*2], even[i*2+1])
+		}
 	}
 
 	out.Flush()
